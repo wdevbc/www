67965631d0f5b68c0c36a9501fed7aa64514e447
wdevbc <wdevbc@gmail.com>
2022-06-09 16:03:34 +0900

    # scroll_percent fix  - javscript resize add

--- javascript/dev/scroll_percent/common.js
+++ javascript/dev/scroll_percent/common.js
@@ -1,12 +1,15 @@
-$browserHeight = $(window).height();
-$(document).ready(function () {
-    $height = $(document).height() - $browserHeight;
-    $(window).scroll(function () {
-        $scroll = $(window).scrollTop();
-        $percent = $scroll / $height * 100;
-        $('.scroll_progress').css({
-            width: $percent + '%'
+$(document).ready(function(){
+    function scroll_progress(){
+        $browserHeight = $(window).height();
+        $height = $(document).height() - $browserHeight;
+        $(window).scroll(function(){
+            $scroll = $(window).scrollTop();
+            $percent = $scroll / $height * 100;
+            $('.scroll_progress').css({width: $percent + '%'});
         });
-    });
-
+    }
+    scroll_progress();
+    $(window).resize(function(){
+        scroll_progress();
+    })
 });
